851409699ae3deb28d1af4d0501e4372cc3ffcc5
takudeva <126333608+takudeva@users.noreply.github.com>
2023-07-09 21:49:05 +0000

    [Edit8]3-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,23 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @book = Book.new
+    @books = @user.books
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    @user = User.find(params[:id])
+    redirect_to user_path(@user)
   end
 
   def edit
   end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
