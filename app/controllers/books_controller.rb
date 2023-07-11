b52e1a09fce608a165c339c63882552794da3454
takudeva <126333608+takudeva@users.noreply.github.com>
2023-07-11 21:40:43 +0000

    [Edit9]3-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,16 @@ class BooksController < ApplicationController
   def new
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+
   def show
+    # @user = User.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def index
@@ -10,4 +19,16 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,20 +9,7 @@ class UsersController < ApplicationController
     @books = @user.books
   end
 
-  def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    @book.save
-    @user = User.find(params[:id])
-    redirect_to user_path(@user)
-  end
-
   def edit
-  end
-
-  private
-  def book_params
-    params.require(:book).permit(:title, :body)
   end
 
 end
