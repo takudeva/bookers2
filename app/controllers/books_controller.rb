f98397496a0797bc9456ca4c7fc656707e780aec
takudeva <126333608+takudeva@users.noreply.github.com>
2023-07-15 06:47:57 +0000

    [Edit12]3-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,11 +16,17 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
-    # @book = Book.find(params[:id])
     @user = current_user
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
   def destroy
