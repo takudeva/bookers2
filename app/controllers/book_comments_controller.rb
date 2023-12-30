e81038e514141cab8e866d2159a1cc31249035d5
takudeva <126333608+takudeva@users.noreply.github.com>
2023-12-30 09:55:01 +0000

    in making book_comments

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,29 @@
+class BookCommentsController < ApplicationController
+  def create
+    @book = Book.find(params[:book_id])
+    comment = current_user.book_comments.new(book_comment_params)
+    comment.book_id = @book.id
+    if comment.save
+      redirect_to book_path(@book)
+    else
+      @book = Book.find(params[:book_id])
+      @user = @book.user
+      @book_comment = BookComment.new
+      render 'books/show'
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:book_id])
+    comment = current_user.book_comments.find_by(book_id: @book.id)
+    comment.destroy
+    redirect_to book_path(@book)
+  end
+
+  private
+
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'book_comments/create'
+  get 'book_comments/destroy'
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => "homes#about", as: 'about'
@@ -7,6 +9,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :edit, :update]
   resources :books do
     resource :favorite, only: [:create, :destroy]
+    resource :book_comments, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
