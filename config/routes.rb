51f927acd0bfe1e0ebc44473ba1a442c8a82f4be
takudeva <126333608+takudeva@users.noreply.github.com>
2023-12-30 08:23:03 +0000

    make Favorite

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'homes#top'
+  get 'home/about' => "homes#about", as: 'about'
 
   resources :users, only: [:index, :show, :edit, :update]
-  resources :books
-  get 'home/about' => "homes#about", as: 'about'
+  resources :books do
+    resource :favorite, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
