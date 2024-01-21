e7116a1fedf7bd47666882ed5f1340e47bc1edfd
takudeva <126333608+takudeva@users.noreply.github.com>
2024-01-21 09:25:13 +0000

    in making follow_follower function

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy 
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy 
+  has_many :followings, through: "active_relationships", source: "followed"
+  has_many :followers, through: "passive_relationships", source: "follower"
 
   has_one_attached :profile_image
 
@@ -21,4 +25,15 @@ class User < ApplicationRecord
     profile_image.variant(resize_to_limit: [width,height]).processed
   end
 
+  def follow(user)
+    active_relationships.create(followed_id: user.id)
+  end
+
+  def unfollow(user)
+    active_relationships.find_by(followed_id: user.id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
-  get 'book_comments/create'
-  get 'book_comments/destroy'
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => "homes#about", as: 'about'
 
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+      get "followings" => "relationships#followings", as: "followings"
+      get "followers" => "relationships#followers", as: "followers"
+  end
   resources :books do
     resource :favorite, only: [:create, :destroy]
     resource :book_comments, only: [:create, :destroy]
