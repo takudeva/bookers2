4b05a42fe5ede30f4de956d56b01e6da06c4b7bd
takudeva <126333608+takudeva@users.noreply.github.com>
2024-01-27 10:05:49 +0000

    made search function

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => "homes#about", as: 'about'
-
+  get 'search' => "searches#search"
+  
   resources :users, only: [:index, :show, :edit, :update] do
     resource :relationships, only: [:create, :destroy]
       get "followings" => "relationships#followings", as: "followings"
